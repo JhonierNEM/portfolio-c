47deffa2173bdd86ce47bb98cc2060264babf831
jhoniernem <jhonny30ca@gmail.com>
2023-03-25 09:35:51 -0500

    Feat: Add H_Title component for section headings

---
+++ src/components/headers/H_Title.jsx
@@ -0,0 +1,8 @@
+export default function H_Title({ title, sub, className }) {
+  return (
+    <div className="flex flex-col items-center justify-center pt-6 pb-5 md:pt-[5.5rem]">
+      <h1 className="text-3xl font-bold">{title}</h1>
+      <p className="text-textThree">{sub}</p>
+    </div>
+  );
+}
